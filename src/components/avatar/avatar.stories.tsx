50fb18cc97d9ae624a81386ee960e5264ab381a1
mc <matthew@cochranweb.com>
2021-02-25 22:18:53 -0500

    start fleshing out stories

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -8,6 +8,48 @@ image="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2
 alt="Gray tabby kitten looking down"
 ></v-avatar>`
 
+export const Initials = () => `<v-avatar initials="VE"></v-avatar>`
+
+export const CustomIcons = () => `<v-avatar>
+<v-icon slot="icon" name="image"></v-icon>
+</v-avatar>
+
+<v-avatar>
+<v-icon slot="icon" name="archive"></v-icon>
+</v-avatar>
+
+<v-avatar>
+<v-icon slot="icon" name="briefcase"></v-icon>
+</v-avatar>
+`
+
+export const Shapes = () => `<v-avatar shape="square"></v-avatar>
+
+<v-avatar shape="rounded"></v-avatar>
+
+<v-avatar shape="circle"></v-avatar>
+`
+
+export const Groups = () => `<div class="avatar-group">
+<v-avatar image="https://images.unsplash.com/photo-1490150028299-bf57d78394e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&q=80&crop=right"></v-avatar>
+<v-avatar image="https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=left&q=80"></v-avatar>
+<v-avatar image="https://images.unsplash.com/photo-1456439663599-95b042d50252?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=left&q=80"></v-avatar>
+<v-avatar image="https://images.unsplash.com/flagged/photo-1554078875-e37cb8b0e27d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=top&q=80"></v-avatar>
+</div>
+
+<style>
+.avatar-group v-avatar:not(:first-of-type) {
+  margin-left: -1rem;
+}
+
+.avatar-group v-avatar::part(base) {
+  border: solid 2px white;
+}
+</style>
+`
+
+
+
 // Basic.args = { 
 //     first: 'stencil', 
 //     middle:'storybook',
--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -2,9 +2,56 @@ export default {
     title: 'Vetrify/Badge'
 }
 
-export const Basic = () => 
+export const Basic = () => `<v-badge>Badge</v-badge>`
+
+export const Types = () =>
 `<v-badge type="primary">Primary</v-badge>
 <v-badge type="success">Success</v-badge>
 <v-badge type="info">Info</v-badge>
 <v-badge type="warning">Warning</v-badge>
 <v-badge type="danger">Danger</v-badge>`
+
+export const Pills = () => `<v-badge type="primary" pill>Primary</v-badge>
+<v-badge type="success" pill>Success</v-badge>
+<v-badge type="info" pill>Info</v-badge>
+<v-badge type="warning" pill>Warning</v-badge>
+<v-badge type="danger" pill>Danger</v-badge>
+`
+
+export const Pulsating = () => `<div class="badge-pulse">
+<v-badge type="primary" pill pulse>1</v-badge>
+<v-badge type="success" pill pulse>1</v-badge>
+<v-badge type="info" pill pulse>1</v-badge>
+<v-badge type="warning" pill pulse>1</v-badge>
+<v-badge type="danger" pill pulse>1</v-badge>
+</div>
+
+<style>
+.badge-pulse v-badge:not(:last-of-type) {
+  margin-right: 1rem;
+}
+</style>
+`
+
+export const WithButtons = () => `<v-button>
+Requests
+<v-badge pill>30</v-badge>
+</v-button>
+
+<v-button style="margin-left: 1rem;">
+Warnings
+<v-badge type="warning" pill>8</v-badge>
+</v-button>
+
+<v-button style="margin-left: 1rem;">
+Errors
+<v-badge type="danger" pill>6</v-badge>
+</v-button>
+`
+
+export const WithMenuItems = () => `<v-menu style="max-width: 240px; border: solid 1px var(--v-panel-border-color); border-radius: var(--v-border-radius-medium);">
+<v-menu-label>Messages</v-menu-label>
+<v-menu-item>Comments <v-badge slot="suffix" pill>4</v-badge></v-menu-item>
+<v-menu-item>Replies <v-badge slot="suffix" pill>12</v-badge></v-menu-item>
+</v-menu>
+`
